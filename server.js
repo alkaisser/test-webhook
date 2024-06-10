821336aa3155b436f43f4e88a43b977a502525a4
Alfredo Lara <alfredo146.al@gmail.com>
2024-06-09 22:41:53 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const { join } = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    wssEngine: ["ws", "wss"],
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        transports: ["websocket", "polling"],
+        credentials: true
+    },
+    allowEIO3: true,
+    pingTimeout: 60000,
+});
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    console.log('Webhook recibido:', req.body);
+    io.emit('webhookEvent', req.body);
+    res.sendStatus(200);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('Cliente conectado:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado:', socket.id);
+    });
+});
